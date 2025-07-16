90061c8f781a5c41dca18483e67e4f616d400168
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-07-16 12:55:59 +0200

    Create a function that completes the exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let result = "";
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      result += str;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
