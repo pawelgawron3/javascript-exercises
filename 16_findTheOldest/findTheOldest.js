c0eda4dafea2451d42ac3c669f7dae97bafecca7
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-07-21 14:10:57 +0200

    Write the function logic

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  return arr
+    .map((person) => {
+      if (person.yearOfDeath === undefined) {
+        person.yearOfDeath = new Date().getFullYear();
+      }
+      let yearsLived = person.yearOfDeath - person.yearOfBirth;
+      return { name: person.name, yearsLived: yearsLived };
+    })
+    .sort((a, b) => b.yearsLived - a.yearsLived)[0];
 };
 
 // Do not edit below this line
