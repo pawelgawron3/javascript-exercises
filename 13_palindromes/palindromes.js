ee8861c45735f4c61ad57e165aa6ec2851f30f9c
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-07-21 13:11:58 +0200

    Create the function logic

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+  let arr = [...cleanStr];
+
+  for (let i = 0, j = arr.length - 1; i < j; i++, j--) {
+    if (arr[i] === arr[j]) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
