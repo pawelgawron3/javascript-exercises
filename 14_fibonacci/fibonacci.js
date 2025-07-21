03a5aa5775f5890f16dd9dd4c2e1c5b8cb773ecf
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-07-21 13:33:59 +0200

    Write a fib function for the exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  //1, 1, 2, 3, 5, 8...
+  if (num == 0) {
+    return 0;
+  } else if (num < 0) {
+    return "OOPS";
+  } else {
+    let arr = [1, 1];
+    for (let i = 2; i < num; i++) {
+      arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr.at(-1);
+  }
 };
 
 // Do not edit below this line
