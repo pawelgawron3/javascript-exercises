4b5152290e1ef6baa8ca3d435d04412b4d4b43ca
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-07-21 12:44:26 +0200

    Update the functions logic

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((acc, val) => (acc += val), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, val) => (acc *= val), 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let k = 1;
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    for (let i = num; i >= 2; i--) {
+      k *= i;
+    }
+    return k;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +37,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
